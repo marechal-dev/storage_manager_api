c3facf28e25d0a7a56fdaeeac5d3a545a6c911e4
Pietro Piva Vieira <pietro.developer@gmail.com>
2022-03-06 23:42:52 -0300

    wip

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -23,20 +23,8 @@ class ProductsController {
     }
   }
 
-  async handleEditRequest(request, response) {
-    const { id, title, price, quantity } = request.body;
-
-    try {
-      await mongoData.updateProduct(id, title, price, quantity);
-
-      return response.status(200).send();
-    } catch (error) {
-      return response.status(500).send(error);
-    }
-  }
-
   async handleDeleteRequest(request, response) {
-    const { id } = request.body;
+    const { id } = request.query;
 
     try {
       await mongoData.deleteProduct(id);
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -7,18 +7,14 @@ const productsRoutes = Router();
 
 productsRoutes.get("/", (request, response) => {
   return productsController.handleListRequest(request, response);
-})
+});
 
 productsRoutes.post("/", (request, response) => {
   return productsController.handleCreateRequest(request, response);
-})
-
-productsRoutes.put("/:id", (request, response) => {
-  return productsController.handleEditRequest(request, response);
-})
+});
 
 productsRoutes.delete("/:id", (request, response) => {
   return productsController.handleDeleteRequest(request, response);
-})
+});
 
 module.exports = { productsRoutes };
